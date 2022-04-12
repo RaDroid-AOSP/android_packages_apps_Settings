c61bcb33678f5f7e6b4c07341701af05402f2f30
Anushek Prasal <anushekprasal@gmail.com>
2022-04-12 13:12:46 +0800

    Settings: Introduce automated Sleep Mode [2/2]
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Change-Id: I00f8f92dc9f90f0c8b2859ce9c494fab400f9a9d
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -397,6 +397,7 @@ public class SettingsGateway {
             ScreenTimeoutSettings.class.getName(),
             ResetNetwork.class.getName(),
             VibrationIntensitySettingsFragment.class.getName(),
+            SleepMode.class.getName(),
             CellularSecuritySettingsFragment.class.getName(),
             AccessibilityHearingAidsFragment.class.getName(),
             HearingDevicePairingFragment.class.getName(),
@@ -448,5 +449,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.SleepModeActivity.class.getName(),
     };
 }
